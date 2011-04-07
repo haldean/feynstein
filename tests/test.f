1e1067365aed318c78ab4484ee0909c330fbaedd
Will Brown <will.h.brown@gmail.com>
2011-04-07 12:05:34 -0400

    Updated tests to include comments, print statement.

--- tests/test.f
+++ tests/test.f
@@ -1,19 +1,24 @@
 MyScene {
-    public void test(int i, int j) {
+    public int sum(int i, int j) {
+	return i + j;
     }
 	
     shapes {
 	shape Cylinder(radius=10, location=(20, 30));
 	booleanFunc(willit==work);
-	int abcdefg = 4;
-	xyz { 
-	    #myShape;
-	};
+
+	if (4 + 2 = 6) {
+	    // a line comment
+	    print("Hey look!"); // sneaky comment.
+	}
     }
 
+    /**
+     * Creates some super-special forces.
+     */
     forces {
 	force Force(location=(123, 123), someProperty=aVariable);
-	Shape(asdf="anotherTest");
+	Shape(asdf="#anotherTest");
     }
 
     onFrame {
