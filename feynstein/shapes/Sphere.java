72652765955d01a3d16c18eb24845e797f833a0d
Will Brown <will.h.brown@gmail.com>
2011-04-24 19:12:10 -0400

    Sphere is now manifold as well. Huzzah!

--- feynstein/shapes/Sphere.java
+++ feynstein/shapes/Sphere.java
@@ -36,10 +36,6 @@ public class Sphere extends Shape<Sphere> {
 	int index = -1, disc_start = 0, last_disc = -1;
 
 	for (int disc_index=0; disc_index < circle_verts+1; disc_index++) {
-	    /* Caps are treated differently because they are connected
-	     * to their center point. */
-	    boolean is_cap = disc_index == 0;
-
 	    /* Calculate the radius of this disc. */
 	    double disc_r = Math.sqrt(Math.pow(radius, 2) - 
 				      Math.pow(2 * radius * disc_index / circle_verts - radius, 2));
@@ -50,44 +46,50 @@ public class Sphere extends Shape<Sphere> {
 	    /* If this disc is a cap, then we have to add its
 	     * centerpoint to the mesh. If it is not a cap, then
 	     * we do not add the center. */
-	    if (is_cap) {
-		disc_start = particles.size();
-		particles.add(new Particle(center));
-	    } else {
-		disc_start = particles.size() - 1;
-	    }
-
+	    disc_start = particles.isEmpty() ? -1 : particles.size() - 1;
+		
 	    /* Generate this disc by sweeping a circle. */
-	    for (int i=0; i<circle_verts; i++) {
+	    for (int i=1; i<=circle_verts; i++) {
 		index = particles.size();
 		point = center.plus(new Vector3d(disc_r * Math.cos(theta*i),
 						 disc_r * Math.sin(theta*i), 0));
 		particles.add(new Particle(point));
-		
-		if (i > 0) {
-		    /* If this is not the first point in this disc,
-		     * add an edge to its previous neighbor. */
-		    edges.add(new Edge(index-1, index));
 
-		    /* If this disc is a cap, add a triangle between
-		     * the current point, its previous neighbor and
-		     * the center of the disc. */
-		    if (is_cap) {
-			triangles.add(new Triangle(disc_start, index-1, index));
-		    } else {
-			edges.add(new Edge(last_disc+i+1, index));
-			triangles.add(new Triangle(last_disc+i, last_disc+i+1, index));
-			triangles.add(new Triangle(last_disc+i, index-1, index));
+		/* Add an edge to the one that came before it in this
+		 * disc. */
+		if (i > 1) {
+		    edges.add(new Edge(index-1, index));
+		}
+
+		/* If there was a disc before this one. */
+		if (disc_start >= 0) {
+		    /* Add an edge to the corresponding point in the
+		     * previous disc. */
+		    edges.add(new Edge(last_disc+i, index));
+
+		    /* If there is a point before this in the disc */
+		    if (i > 1) {
+			/* Add an edge along the diagonal of the face
+			 * and add the two triangles that make up the
+			 * face. */
+			edges.add(new Edge(last_disc+i-1, index));
+			triangles.add(new Triangle(last_disc+i-1, index-1, index));
+			triangles.add(new Triangle(last_disc+i-1, last_disc+i, index));
 		    }
-		} else {
-		    edges.add(new Edge(last_disc+circle_verts, index));
-		    triangles.add(new Triangle(last_disc+1, last_disc+circle_verts, index));
 		}
 	    }
 
-	    edges.add(new Edge(disc_start+1, index));
-	    if (is_cap) {
+	    /* Create the missing face (the one that links the
+	     * starting points and ending points of each face) */
+	    if (disc_start >= 0) {
+		edges.add(new Edge(disc_start, disc_start+1));
+		triangles.add(new Triangle(last_disc+1, disc_start, disc_start+1));
 		triangles.add(new Triangle(disc_start, disc_start+1, index));
+	    }
+
+	    edges.add(new Edge(disc_start+1, index));
+	    if (disc_start > 0) {
+		edges.add(new Edge(disc_start, index));
 	    }
 
 	    last_disc = disc_start;
