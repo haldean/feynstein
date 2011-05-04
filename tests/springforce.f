f71c83a65b3275466a8ef2774dfc8a369362c846
Sam <sla2120@columbia.edu>
2011-05-04 19:54:21 -0400

    changed this experimental test here and there

--- tests/springforce.f
+++ tests/springforce.f
@@ -1,7 +1,7 @@
 MyScene {
     shapes {
-	//shape CustomObject(name="obj1", file="scenes/BunnyCrusher.obj");
-	shape SpringChain(name="obj1", vert=(0,20,0), vert=(0,10,0), vert=(0,0,0), fixed=0);
+	shape CustomObject(name="obj1", file="scenes/BunnyCrusher.obj");
+	//shape SpringChain(name="obj1", vert=(0,20,0), vert=(0,10,0), vert=(0,0,0), fixed=0);
 	// Nested blocks
 	if (4 newton + 2.7e10 forcelb == 6 dyne) {
 	    print("Something happened");
@@ -12,8 +12,10 @@ MyScene {
      * Creates some super-special forces.
      */
     forces {
-	force SpringForce(actsOn=#obj1, strength=50, length=10);
-	force GravityForce(gy=-9.8);
+	force SpringForce(actsOn=#obj1, strength=10, length=1);
+	force TriangleForce(actsOn=#obj1, youngs=1, poisson=0.1);
+	force SurfaceBendingForce(actsOn=#obj1);
+	//force GravityForce(gy=-9.8);
     }
 
     properties { 
