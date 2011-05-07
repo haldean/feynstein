f0fa155f0861253598625ce2e87f4a0c2acedeb6
Colleen McKenzie <colleen.mckenzie@gmail.com>
2011-05-07 18:31:48 -0400

    ...and here's the version of that script that isn't broken.

--- contribs2.sh
+++ contribs2.sh
@@ -1,3 +1,1 @@
-find feynstein/ translator/ tests/ -name '*.*' | \
-     xargs -If \     
-     git blame -c -w f | perl contribs2.pl
+find feynstein/ translator/ tests/ -name '*.*' | xargs -If git blame -c -w f | perl contribs2.pl
