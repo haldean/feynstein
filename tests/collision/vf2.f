72ed0732dcbd860701d6e38e491c0c32e6d5e138
Colleen McKenzie <colleen.mckenzie@gmail.com>
2011-05-08 15:43:34 -0400

    More collision tests

---
+++ tests/collision/vf2.f
@@ -0,0 +1,19 @@
+MyScene {
+   shapes {
+      shape TriangleShape(name="tri1", vert=(0,2.88,10.5), vert=(-5,2.88,20.5), vert=(5,2.88,20.5), velocity=(0,0,-1), velocity=(0,0,-1), velocity=(0,0,-1));
+      shape TriangleShape(name="tri2", vert=(-5,0,0), vert=(0,8.66,0), vert=(5,0,0));
+    }
+
+    
+    forces {
+      force TriangleForce(actsOn=#tri1, youngs=10.0, poisson=.1);
+      force TriangleForce(actsOn=#tri2, youngs=10.0, poisson=.1);
+    }
+
+    properties { 
+        property SemiImplicitEuler(stepSize=0.01);
+        //property BoundingVolumeHierarchy(margin=0.1);
+        property ProximityDetector(proximity=0.1);
+        property ImpulseResponder(detector=0, iterations=100);
+   }
+}
