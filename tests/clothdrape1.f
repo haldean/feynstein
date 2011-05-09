5f33627171b45e11e6e0e7bb3260e809ec356c03
Colleen McKenzie <colleen.mckenzie@gmail.com>
2011-05-09 18:10:25 -0400

    Changes to cloth drape demo

--- tests/clothdrape1.f
+++ tests/clothdrape1.f
@@ -2,7 +2,8 @@ ClothScene {
     boolean flip = false;
     shapes {
 	shape FluidPlane(name="cloth", subdivisions=20, length=30, velocity=(0,-.2,0));
-	shape Sphere(name="sphere", accuracy=20, radius=5, location=(10,-12,10));
+	//shape Sphere(name="sphere", accuracy=20, radius=5, location=(10,-12,10));
+	shape Cube(name="cube", sides=(10,10,10), location=(10,-12,10));
     }
 
     /**
@@ -18,9 +19,9 @@ ClothScene {
 	 property SemiImplicitEuler(stepSize=0.005);
 	property BoundingVolumeHierarchy(margin=0.02);
 	//property ContinuousTimeDetector(stepSize=0.01);
-	//property ImpulseResponder(detector=0, iterations=100);
-	property ProximityDetector(proximity=0.01);
-	property SpringPenaltyResponder(detector=0, stiffness=500);
+	property ProximityDetector(proximity=0.001);
+	property ImpulseResponder(detector=0, iterations=100);
+	//property SpringPenaltyResponder(detector=0, stiffness=10);
 	//property SemiImplicitEuler(stepSize=0.01);
    }
 
