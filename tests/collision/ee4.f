03f67f2e62f3aa370c860e71d29a802459536ec1
Colleen McKenzie <colleen.mckenzie@gmail.com>
2011-05-09 16:23:41 -0400

    Ensuring collision tests are updated

--- tests/collision/ee4.f
+++ tests/collision/ee4.f
@@ -12,7 +12,7 @@ MyScene {
 
     properties { 
       property SemiImplicitEuler(stepSize=.01);
-        property BoundingVolumeHierarchy(margin=0.1);
+//        property BoundingVolumeHierarchy(margin=0.1);
         property ProximityDetector(proximity=0.1);
         property ImpulseResponder(detector=0, iterations=100);
    }
