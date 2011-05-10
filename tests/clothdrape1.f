51a374335e61f0569e75dae7a5cddab25db4b5f1
Sam <sla2120@columbia.edu>
2011-05-10 16:03:04 -0400

    MOTHERFUCKINGCLOTHDRAPE

--- tests/clothdrape1.f
+++ tests/clothdrape1.f
@@ -1,7 +1,7 @@
 ClothScene {
     boolean flip = false;
     shapes {
-	shape FluidPlane(name="cloth", subdivisions=20, length=30);
+	shape FluidPlane(name="cloth", subdivisions=10, length=30);
 	//shape Sphere(name="sphere", accuracy=20, radius=5, location=(10,-12,10));
 	shape Cube(name="cube", sides=(10,10,10), location=(10,-10.5,10), fixed=true);
 	#cloth.translate(0,0,0);
@@ -14,7 +14,7 @@ ClothScene {
     forces {
 	force GravityForce(gy=-2.0);
 	//force DampingForce(coefficient=0.2);
-	force TriangleForce(actsOn=#cloth, youngs=100, poisson=0.1);
+	force TriangleForce(actsOn=#cloth, youngs=10, poisson=0.1);
     }
 
     properties { 
@@ -22,8 +22,9 @@ ClothScene {
 	 //property BoundingVolumeHierarchy(margin=0.02);
 	 //property ContinuousTimeDetector(stepSize=0.01);
 	property ProximityDetector(proximity=0.01);
-	//property ImpulseResponder(detector=0, iterations=10);
-	property SpringPenaltyResponder(detector=0, stiffness=10);
+	//property ImpulseResponder(detector=0, iterations=100);
+	property SpringPenaltyResponder(detector=0, stiffness=50, proximity=0.01);
+	property ImpulseResponder(detector=0, iterations=10);
 	//property SemiImplicitEuler(stepSize=0.01);
    }
 
