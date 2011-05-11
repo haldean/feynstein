6a00a567bc0d7ea57aebc34294aa813ee32d02da
Will Brown <will.h.brown@gmail.com>
2011-05-11 01:57:00 -0400

    Fixed some import bugs causing feynstein.Shape to clash with something in java.awt, added initialize method for cheating

--- translator/imports.py
+++ translator/imports.py
@@ -3,9 +3,9 @@
 imports = [
     'feynstein.shapes.*', 'feynstein.forces.*', 'feynstein.*',
     'feynstein.renderer.*', 'feynstein.properties.*', 
-    'feynstein.properties.integrators.*', 'java.awt.*', 'java.awt.event.*',
+    'feynstein.properties.integrators.*', 'java.awt.Frame', 'java.awt.event.*',
     'javax.media.opengl.awt.*', 'com.jogamp.opengl.util.*', 
-    'feynstein.properties.collision.*',
+    'feynstein.properties.collision.*', 'java.util.*',
     ]
 
 def get_imports():
